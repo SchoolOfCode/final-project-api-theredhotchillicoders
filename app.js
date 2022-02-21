81756a2738a06c2d78f689b734abbc174294f940
Leanne251 <leannesmith251@gmail.com>
2022-02-21 15:48:46 +0000

    updated paths with activities and added isComplete Co-authored-by: Leanne leannesmith251@gmail.com Co-authored-by: Tan tanmatthews1@gmail.com Co-authored-by: Emily emy.sarina@gmail.com Co-authored-by: Mohammed ali.m2804@outlook.com Co-authored-by: troy troywebb95@gmail.com Co-authored-by: jack jackfirkin@gmail.com

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import path from 'path';
 
-import __dirname  from './dirname.js';
-import cookieParser  from 'cookie-parser';
-import cors  from 'cors';
-import logger  from 'morgan';
+import __dirname from './dirname.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import logger from 'morgan';
 
-import usersRouter  from './routes/users.js';
+import usersRouter from './routes/activities.js';
 
 const app = express();
 
@@ -15,17 +15,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', usersRouter);
+app.use('/activities', usersRouter);
 
-app.use(function (req, res, next) {
-  res.status(404).json({message: "We couldn't find what you were looking for 😞"})
-})
+app.use(function(req, res, next) {
+	res.status(404).json({ message: "We couldn't find what you were looking for 😞" });
+});
 
-app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).json(err)
-})
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500).json(err);
+});
 
 export default app;
