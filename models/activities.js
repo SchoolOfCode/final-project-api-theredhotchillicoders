0d2cd344f0b63b4a785075182429cec6cd18261b
Leanne251 <leannesmith251@gmail.com>
2022-02-28 18:24:54 +0000

    adding patch

--- models/activities.js
+++ models/activities.js
@@ -1,40 +1,41 @@
-import db from "../db/connection.js";
+import db from '../db/connection.js';
 //changed to activities
 
 export async function getAllActivities() {
-  const result = await db.query(`SELECT * FROM activities;`);
-  return result.rows;
+	const result = await db.query(`SELECT * FROM activities;`);
+	return result.rows;
 }
 
 export async function getActivitiesByUser(userID) {
-  const result = await db.query(`SELECT * FROM activities WHERE userID= $1;`, [
-    userID,
-  ]);
-  return result.rows;
+	const result = await db.query(`SELECT * FROM activities WHERE userID= $1;`, [ userID ]);
+	return result.rows;
 }
 
-export async function createData({
-  date,
-  title,
-  category,
-  description,
-  duration,
-  userid,
-}) {
-  const data = await db.query(
-    `INSERT INTO activities(date, title, category, description, duration, userid) VALUES($1, $2, $3, $4, $5, $6) 
+export async function createData({ date, title, category, description, duration, userid }) {
+	const data = await db.query(
+		`INSERT INTO activities(date, title, category, description, duration, userid, isComplete) VALUES($1, $2, $3, $4, $5, $6, $7) 
     RETURNING title;`,
-    [date, title, category, description, duration, userid]
-  );
+		[ date, title, category, description, duration, userid, false ]
+	);
 
-  return data.rows;
+	return data.rows;
 }
 
 export async function deleteTaskById(id) {
-  const data = await db.query(
-    `DELETE FROM activities
-  WHERE id= $1`,
-    [id]
-  );
-  return `Deleted ${id}`;
+	const data = await db.query(
+		`DELETE FROM activities
+  WHERE id= $1 `,
+		[ id ]
+	);
+	return `Deleted ${id}`;
+}
+
+export async function changeComplete(id) {
+	const data = await db.query(
+		`UPDATE activities
+  SET isComplete =  NOT isComplete
+  WHERE id=  $1 
+  RETURNING title;`,
+		[ id ]
+	);
 }
--- routes/activities.js
+++ routes/activities.js
@@ -1,54 +1,69 @@
-import express from "express";
+import express from 'express';
 import {
-  getAllActivities,
-  createData,
-  deleteTaskById,
-  getActivitiesByUser,
-} from "../models/activities.js";
+	getAllActivities,
+	createData,
+	deleteTaskById,
+	getActivitiesByUser,
+	changeComplete
+} from '../models/activities.js';
 //changed to activities
 
 const router = express.Router();
 
 /* GET all activities. */
-router.get("/", async function (req, res, next) {
-  const activities = await getAllActivities();
+router.get('/', async function(req, res, next) {
+	const activities = await getAllActivities();
 
-  res.json({
-    success: true,
-    payload: activities,
-  });
+	res.json({
+		success: true,
+		payload: activities
+	});
 });
 
 /* GET activities for a SPECIFIC user */
-router.get("/user", async function (req, res, next) {
-  const userID = req.user.uid;
-  const activities = await getActivitiesByUser(userID);
-  res.json({
-    success: true,
-    payload: activities,
-  });
+router.get('/user', async function(req, res, next) {
+	const userID = req.user.uid;
+	const activities = await getActivitiesByUser(userID);
+	res.json({
+		success: true,
+		payload: activities
+	});
 });
 
-router.post("/", async function (req, res, next) {
-  const body = req.body;
-  const create = await createData(body);
+router.post('/', async function(req, res, next) {
+	const body = req.body;
+	const create = await createData(body);
 
-  res.json({
-    success: true,
-    payload: create,
-  });
+	res.json({
+		success: true,
+		payload: create
+	});
 });
 
-router.delete("/:id", async function (req, res, next) {
-  const id = req.params.id;
-  const remove = await deleteTaskById(id);
-  console.log(id);
-  res.json({
-    success: true,
-    payload: remove,
-  });
+router.delete('/:id', async function(req, res, next) {
+	const id = req.params.id;
+	const remove = await deleteTaskById(id);
+	console.log(id);
+	res.json({
+		success: true,
+		payload: remove
+	});
 
-  // deleteTaskById(id);
+	// deleteTaskById(id);
+});
+
+router.patch('/:id', async function(req, res, next) {
+	const id = req.params.id;
+	const complete = await changeComplete(id);
+	console.log(id);
+	res.json({
+		success: true,
+		payload: complete
+	});
+
+	// deleteTaskById(id);
 });
 
 export default router;
+
+//
