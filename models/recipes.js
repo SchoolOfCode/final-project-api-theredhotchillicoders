954bb50d4b37b9f17de5024c059654c43b6259e8
firko-learnin <jackfirkin@gmail.com>
2022-03-01 10:57:33 +0000

    Add search for recipe by ID Co-authored-by: Mohammed Ali.m2804@outlook.com Co-authored-by: Troy troywebb95@gmail.com

--- models/recipes.js
+++ models/recipes.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
+const apiKey = process.env.RECIPE_API_KEY;
 
 export async function getRecipe(recipe) {
-  const apiKey = process.env.RECIPE_API_KEY;
   const res = await fetch(
     `https://api.spoonacular.com/recipes/complexSearch?query=${recipe}&sort=popularity&number=6`, //&apiKey=${apiKey}
     {
@@ -9,6 +9,21 @@ export async function getRecipe(recipe) {
       headers: { "Content-Type": "application/json" },
     }
   );
+  //Returns a list of recipes
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+
+export async function getRecipeDetails(recipeID) {
+  const res = await fetch(
+    `https://api.spoonacular.com/recipes/${recipeID}/information?includeNutrition=false`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  //Returns details for a single recipe
   const data = await res.json();
   console.log(data);
   return data;
--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -1,15 +1,25 @@
 import express from "express";
-import { getRecipe } from "../models/recipes.js";
+import { getRecipe, getRecipeDetails } from "../models/recipes.js";
 //changed to activities
 
 const router = express.Router();
 
-/* GET all activities. */
-router.get("/", async function (req, res, next) {
+/* GET all recipes. */
+router.get("/search", async function (req, res, next) {
   const recipes = await getRecipe(req.body.recipes);
   res.json({
     success: true,
     payload: recipes,
+  });
+});
+
+/* GET details for a specific recipe */
+router.get("/:id", async function (req, res, next) {
+  const id = req.params.id;
+  const recipe = await getRecipeDetails(id);
+  res.json({
+    success: true,
+    payload: recipe,
   });
 });
 
