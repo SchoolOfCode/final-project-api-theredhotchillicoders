bf8764d1cb062fea59e4dcef5480d910ee94ce4b
firko-learnin <jackfirkin@gmail.com>
2022-03-01 10:04:06 +0000

    Fix recipe API call

--- models/recipes.js
+++ models/recipes.js
@@ -1,15 +1,15 @@
 import fetch from "node-fetch";
 
 export async function getRecipe(recipe) {
-    const apiKey = process.env.RECIPE_API_KEY;
-    const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${recipe}&sort=popularity&number=6`, {
-        method: 'GET',
-        body: JSON.stringify(recipe),
-        headers: { 'Content-Type': 'application/json' }
-    });
-    const data = await res.json();
-    console.log(data);
-
-    return data;
+  const apiKey = process.env.RECIPE_API_KEY;
+  const res = await fetch(
+    `https://api.spoonacular.com/recipes/complexSearch?query=${recipe}&sort=popularity&number=6`, //&apiKey=${apiKey}
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  const data = await res.json();
+  console.log(data);
+  return data;
 }
-
